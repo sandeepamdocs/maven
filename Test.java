1c561d665f5a2f8ff11b39f30c3dcee774af3edb
rajaramthumati <35828671+rajaramthumati@users.noreply.github.com>
2019-01-29 09:46:04 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -5,6 +5,7 @@ public static void main(String[] args)
 {
 System.out.println("Hello this is sample code");
  System.out.println("hi this is sample code"); 
- System.out.println("hi this is test code"); 
+ System.out.println("hi this is test code");
+ System.out.println("hello this is test code");
 }
 }
