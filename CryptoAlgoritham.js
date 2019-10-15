ef096d3357e12f9eeb4e5e07221bdc24816661c4
MooleMuraliDharaReddy <42165328+MooleMuraliDharaReddy@users.noreply.github.com>
2019-10-15 22:30:03 +0530

    Update CryptoAlgoritham.js

--- CryptoAlgoritham.js
+++ CryptoAlgoritham.js
@@ -3,11 +3,17 @@ var algoritham = 'aes-256-cbd';
 var encryptionpassword ='encpasswd';
 function decryptPassword (messageToDecrpt)
 {
-  let decipher = crypto.createDecipher(algoritham, encryptionPasswprd);
+  let decipher = crypto.createDecipher(algoritham, encryptionPassword);
   let dec = decipher.update(messageToDecrypt,'hex','UTF8');
   dec+ = decipher.final('UTF8');
   return dec;
 }
 function encryptpassword(messageToEncrypt)
 {
-  
+   let cipher = crypto.createCipher(algoritham, encryptionPassword);
+  let crypted = cipher.update(messageToEncrypt,'hex','UTF8');
+  crypted+ = cipher.final('hex');
+  return crypted;
+}
+var cryptedPswd = encryptPasswrd('Amma@143');
+console.log('Encrypted password is: '+cryptedPswd);
