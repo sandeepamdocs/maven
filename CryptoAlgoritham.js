fda0b32bd50823694a3df3f8f952a488b56000de
MooleMuraliDharaReddy <42165328+MooleMuraliDharaReddy@users.noreply.github.com>
2019-10-15 22:25:23 +0530

    Update CryptoAlgoritham.js

--- CryptoAlgoritham.js
+++ CryptoAlgoritham.js
@@ -1,1 +1,13 @@
-
+var crypto = require('crypto');
+var algoritham = 'aes-256-cbd';
+var encryptionpassword ='encpasswd';
+function decryptPassword (messageToDecrpt)
+{
+  let decipher = crypto.createDecipher(algoritham, encryptionPasswprd);
+  let dec = decipher.update(messageToDecrypt,'hex','UTF8');
+  dec+ = decipher.final('UTF8');
+  return dec;
+}
+function encryptpassword(messageToEncrypt)
+{
+  
